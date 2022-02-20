72b0cad5f4b61e9fd70253d81b46538003eda1ee
Amin.Nakhaie <amin.nakhaie@gmail.com>
2022-02-20 19:31:54 +0330

    First Push

---
+++ Bunny Sorter/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Bunny_Sorter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(422, 282);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Directory";
+            // 
+            // label4
+            // 
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label4.Location = new System.Drawing.Point(7, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(311, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Sub Version Name";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(7, 89);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(311, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(7, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(409, 50);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Plan Folder Naming Style : P###\r\nFile Naming Style : P###.{Image Layer}.{Frame Nu" + "m}\r\n # = Number";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(408, 71);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Sort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Address";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(312, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(410, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Root";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(441, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(422, 426);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Log";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(410, 398);
+            this.label2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.GroupBox groupBox1;
+
+        #endregion
+    }
+}
---
+++ Bunny Sorter/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Bunny_Sorter
+{
+    public partial class Form1 : Form
+    {
+        private string _rootPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog folderBrowser = new OpenFileDialog();
+            
+        // Set validate names and check file exists to false otherwise windows will
+        // not let you select "Folder Selection."
+            folderBrowser.ValidateNames = false;
+            folderBrowser.CheckFileExists = false;
+            folderBrowser.CheckPathExists = true;
+            
+        // Always default to Folder Selection.
+            folderBrowser.FileName = "Folder Selection.";
+
+            if (folderBrowser.ShowDialog() != DialogResult.OK)
+                return;
+            
+            _rootPath = Path.GetDirectoryName(folderBrowser.FileName);
+            textBox1.Text = _rootPath;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            _rootPath = textBox1.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            _rootPath = textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = string.Empty;
+            _rootPath   = textBox1.Text;
+            
+            if(_rootPath == string.Empty)
+                return;
+
+            var dirsPath = Directory.GetDirectories(_rootPath);
+            
+            if (dirsPath.Length < 1)
+            {
+                MessageBox.Show(@"There is no plan directory in this path!", @"Incorrect Address", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
+            string errorsDescription = string.Empty;
+
+            Dictionary<string, int> layersList = new Dictionary<string, int>();
+            
+            foreach (var dir in dirsPath)
+            {
+                string[] allFilesPaths = Directory.GetFiles(dir, $"{dir.Split('\\').Last()}.*");
+                
+                if (allFilesPaths.Length < 1)
+                {
+                    errorsDescription += $"{dir} (Empty!)\n";
+                }
+                else
+                {
+                    label2.Text += $"{dir.Split('\\').Last()}\n";
+                    layersList.Clear();
+                    
+                    foreach (var filesPath in allFilesPaths)
+                    {
+                        string planType = filesPath.Split('\\').Last();
+                        string[] planTypeSplit = planType.Split('.');
+
+                        if (planTypeSplit.Length < 3)
+                        {
+                            errorsDescription += $"{dir} (No Types!)\n";
+                            break;
+                        }
+
+                        planType = planTypeSplit[1];
+                        
+                        if (layersList.ContainsKey(planType))
+                        {
+                            layersList[planType]++;
+                        }
+                        else
+                        {
+                            layersList.Add(planType,1);
+                        }
+
+                        string planTypeDir = textBox2.Text;
+                        
+                        planTypeDir = planTypeDir == String.Empty ? $@"{dir}\{planType}" : $@"{dir}\{textBox2.Text}\{planType}";
+
+                        if (!Directory.Exists(planTypeDir))
+                        {
+                            Directory.CreateDirectory(planTypeDir);
+                        }
+
+                        if (planTypeSplit[2] != "0000")
+                        {
+                            planTypeDir = $"{planTypeDir}\\{filesPath.Split('\\').Last()}";
+
+                            File.Move(filesPath, planTypeDir);
+                        }
+                    }
+
+                    foreach (var key in layersList.Keys)
+                    {
+                        label2.Text     += $"   {key} = {layersList[key]}\n";
+                    }
+ 
+                }
+            }
+
+            if (errorsDescription != string.Empty)
+            {
+                errorsDescription = errorsDescription.Insert(0, "There is no plan file in this path! \n");
+                MessageBox.Show(errorsDescription, @"Incorrect Address", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            
+
+        }
+        
+    }
+}
